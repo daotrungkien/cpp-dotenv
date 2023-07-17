8a81c1048fa19d31c7303e438534e2ebb93486d3
Dao Trung Kien <trung-kien.dao@mica.edu.vn>
2023-07-17 09:21:33 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -8,16 +8,16 @@ int main(int argc, const char** argv) {
    // including command-line arguments, environment variables,
     // files (`.env` in current folder, `.env` in program folder,
     // and `test-custom.env` file in program folder)
-    dotenv env1{argc, argv, true, {
+    dotenv env{argc, argv, true, {
         dotenv::in_current_folder(),
         dotenv::in_program_folder(),
         dotenv::in_program_folder("test-custom.env")
     }};
 
-    cout << "PATH = " << env1["PATH"] << endl;
-    cout << "VAR1 = " << env1["VAR1"] << endl;
-    cout << "VAR2 = " << env1["VAR2"] << endl;
-    cout << "VAR3 = " << env1["VAR3"] << endl;
+    cout << "PATH = " << env["PATH"] << endl;
+    cout << "VAR1 = " << env["VAR1"] << endl;
+    cout << "VAR2 = " << env["VAR2"] << endl;
+    cout << "VAR3 = " << env["VAR3"] << endl;
 
     return 0;
 }
